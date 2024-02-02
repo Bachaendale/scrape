4971dc4db12245f3c11e5be22b81277bc7f2f5b3
Bachaendale <113609839+Bachaendale@users.noreply.github.com>
2024-02-03 02:24:38 +0300

    Update test.py

--- test.py
+++ test.py
@@ -9,10 +9,10 @@ CHAT_ID = 304630156
 
 def send_to_telegram(message):
     try:
-        # Construct the Telegram API URL for sending messages
+       
         api_url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage'
         
-        # Prepare the parameters for the API request
+      
         params = {
             'chat_id': CHAT_ID,
             'text': message,
@@ -39,10 +39,10 @@ def scrape_and_send_to_telegram():
        
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        # Find all the headers with class 'entry-header'
+       
         news_headers = soup.find_all('header', class_='entry-header')
 
-        # Prepare a message with the scraped data
+       
         message = ""
         for header in news_headers:
             title_anchor = header.find('h2', class_='entry-title').find('a')
@@ -51,15 +51,15 @@ def scrape_and_send_to_telegram():
                 title_link = title_anchor['href']
                 message += f"<b>Title:</b> {title_text}\n<b>Link:</b> {title_link}\n\n"
 
-        # Send the message to Telegram
+       
         print("Sending message to Telegram...")
         send_to_telegram(message)
 
     else:
         print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
 
-# Run the scraping function every 10 minutes
+
 while True:
     scrape_and_send_to_telegram()
-    # Pause for 10 minutes (600 seconds) before the next iteration
+   
     time.sleep(30)
