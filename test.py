32b55e99e04d8d8fa0470a67bc430d4440f33e8c
Bachaendale <113609839+Bachaendale@users.noreply.github.com>
2024-02-11 21:43:17 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,37 +1,31 @@
-import io
-import sys
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 import time
 
 BOT_TOKEN = '6363247128:AAGb2pd2wpjv121_dSLH1o_zBIGEnIqtJ-E'
 CHAT_ID = 304630156
 
+url = 'https://www.ethiopia-insight.com/category/newsanalysis/'
+
 def send_to_telegram(message):
     try:
-       
         api_url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage'
-        
-      
         params = {
             'chat_id': CHAT_ID,
             'text': message,
             'parse_mode': 'HTML'  
         }
-
-        
+        print("Sending message to Telegram...")
         response = requests.post(api_url, params=params)
-
-      
         if response.status_code != 200:
             print(f"Failed to send message to Telegram. Status code: {response.status_code}")
+        else:
+            print("Message sent to Telegram successfully.")
     except Exception as e:
         print(f"Error sending message to Telegram: {e}")
 
-def scrape_and_send_to_telegram():
-    url = 'https://www.ethiopia-insight.com/category/newsanalysis/'
-
-   
+def scrape_and_send():
+    
     response = requests.get(url)
 
     
@@ -39,27 +33,28 @@ def scrape_and_send_to_telegram():
        
         soup = BeautifulSoup(response.content, 'html.parser')
 
-       
-        news_headers = soup.find_all('header', class_='entry-header')
+    
+        entries = soup.find_all('div', class_='meta-image')
 
-       
-        message = ""
-        for header in news_headers:
-            title_anchor = header.find('h2', class_='entry-title').find('a')
-            if title_anchor:
-                title_text = title_anchor.text.strip()
-                title_link = title_anchor['href']
-                message += f"<b>Title:</b> {title_text}\n<b>Link:</b> {title_link}\n\n"
+        for entry in entries:
+            
+            link = entry.find('a')['href']
 
-       
-        print("Sending message to Telegram...")
-        send_to_telegram(message)
+            
+            entry_title = entry.find_next('h2', class_='entry-title')
+            title_link = entry_title.find('a')['href']
+            title_text = entry_title.text.strip()
+
+            
+            message = f"Link: {link}\nEntry Title: {title_text}\nTitle Link: {title_link}"
+
+          
+            send_to_telegram(message)
 
     else:
-        print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+        print("Failed to retrieve the webpage.")
 
 
-while True:
-    scrape_and_send_to_telegram()
-   
+while True: 
+    scrape_and_send()
     time.sleep(30)
